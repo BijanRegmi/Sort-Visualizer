553e6a9d782b463e65af63ca22b023702ab032e6
Bijan Regmi <vizandabijan@gmail.com>
2021-10-26 23:49:06 +0545

    added status bar

---
+++ src/statusbar.cpp
@@ -0,0 +1,40 @@
+#include "statusbar.h"
+
+Bar::Bar(){}
+
+Bar::Bar(int w, int h, Blocks* b, Algorithms* a){
+    width = w;
+    height = h;
+    data = b;
+    alg = a;
+
+    texture.create(width, height);
+    this->setTexture(texture.getTexture());
+
+    if (!font.loadFromFile("src/font2.ttf"))
+    {
+        std::cout << "Error loading font!";
+    }
+
+    text.setFont(font);
+    text.setCharacterSize(10);
+}
+
+void Bar::render(){
+    std::stringstream sstr;
+
+    sstr << "Selected Alg: "     << alg->getalg()
+         << " | Compare count: " << data->comparecounter
+         << " | Read count: "    << data->readcounter
+         << " | Swap count: "    << data->swapcounter
+         << " | Delay: "         << alg->delay << " microseconds"
+         << " | Data size: "     << data->amount
+         << " | Working: "       << (alg->working ? "Yes" : "No ")
+         << " | Sorted: "        << (alg->sorted ? "Yes" : "No ") ;
+
+    text.setString(sstr.str());
+
+    texture.clear();
+    texture.draw(text);
+    texture.display();
+}
