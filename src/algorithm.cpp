eb776c5fd76b8ba2494c7a5a06e6b39924a16a4c
Bijan Regmi <vizandabijan@gmail.com>
2021-10-26 23:47:56 +0545

    added check and delays

--- src/algorithm.cpp
+++ src/algorithm.cpp
@@ -3,61 +3,111 @@
 // Constructor
 Algorithms::Algorithms(Blocks* b):data(b){
     selectedAlg = 0;
-    isSorting = false;
+    working = false;
+    sorted = true;
+    algcount = alglist.size();
+
+    setdelay(20);
+
     sortingThread = std::thread(&Algorithms::algo, this);   // Setup the thread
 }
 
 // Thread function
 void Algorithms::algo(){
     while(true){
-        if (!isSorting){
+        if (!working){
             std::cout << "";        // IDK why but this needs to be here. Prolly the thread requires some task to do.
             continue;
         }
 
+        data->reset_counters();
         switch(selectedAlg){
         case 0:
-            Algorithms::shuffle();
+            Algorithms::check();
             break;
         case 1:
-            Algorithms::bubblesort();
+            Algorithms::shuffle();
             break;
         case 2:
+            Algorithms::bubblesort();
+            Algorithms::check();
+            break;
+        case 3:
             Algorithms::mergesort();
+            Algorithms::check();
             break;
         }
-        isSorting = false;
+        working = false;
     }
 }
 
 // Controllers
 void Algorithms::start(){
-    isSorting = true;
+    working = true;
 }
 
 void Algorithms::stop(){
-    isSorting = false;
+    working = false;
     sortingThread.detach();
 }
 
 void Algorithms::setalg(int s){
-    selectedAlg = s;
+    if (s == -1)
+        selectedAlg = (selectedAlg + 1)%algcount;
+    else if (s == -2)
+        selectedAlg = (selectedAlg - 1)%algcount;
+    else
+        selectedAlg = s;
+}
+
+void Algorithms::setdelay(int d){
+    if (d > 0)
+        delay = d;
+    else if (d == -1)
+        delay += 10;
+    else if (d == -2 && delay > 10)
+        delay -= 10;
+    else if (d == -3)
+        delay += 100;
+    else if (d == -4 && delay > 100)
+        delay -=100;
+    else if (d == -5)
+        delay += 1000;
+    else if (d == -6 && delay > 1000)
+        delay -= 1000;
+}
+
+// Data Extractors
+std::string Algorithms::getalg(){
+    return alglist[selectedAlg];
 }
 
 // Sorting Algorithms
+void Algorithms::check(){
+    for (data->c_head = 0; data->c_head < data->amount-1; data->c_head++){
+        if (data->cmp(data->c_head, data->c_head+1) != -1){
+            std::cout << "ERROR at " << data->c_head << std::endl;
+            break;
+        }
+        std::this_thread::sleep_for(std::chrono::microseconds(delay));
+    }
+    sorted = (data->amount-1 == data->c_head) ? true : false;
+}
+
 void Algorithms::shuffle(){
     std::random_shuffle(data->items.begin(), data->items.end());
+    sorted = false;
 }
 
 void Algorithms::bubblesort(){
-    for (int i = 0; i < data->getSize(); i += 1){
-        for (int j = 0; j < data->getSize()-i-1; j += 1){
+    for (int i = 0; i < data->amount; i += 1){
+        for (int j = 0; j < data->amount-i-1; j += 1){
 
             if (data->cmp(j, j+1) == 1){
                 data->b_swap(j, j+1);
             }
 
-            std::this_thread::sleep_for(std::chrono::milliseconds(20));
+            std::this_thread::sleep_for(std::chrono::microseconds(delay));
         }
     }
 }
--- src/algorithm.h
+++ src/algorithm.h
@@ -8,21 +8,28 @@
 #include <algorithm>
 
 #include <iostream>
+#include <string>
 
 class Algorithms{
 private:
     Blocks* data;               // Pointer to array of data to sort
-    bool isSorting;             // Set true when the algo is performing sorting
-    int selectedAlg;            // [0, bubblesort] [1, mergesort]
 
     std::thread sortingThread;  // Thread for sorting the blocks
+
+    unsigned int selectedAlg;   // Currently selected algorithm index from alglist
+
+    int algcount;               // Total number of algs in alglist
 
     void algo();                // Function that keeps running in the thread
 
     // Sorting algorithms
+    void check();
     void shuffle();
     void bubblesort();
     void mergesort();
+
+    // Algorithm list
+    const std::vector<std::string> alglist = {"Check", "Shuffle", "Bubblesort", "Mergesort"};
 public:
     // Constructor
     Algorithms(Blocks* b);
@@ -30,8 +37,18 @@ public:
     // Controllers
     void start();
     void stop();
+
+    bool working;               // Set true when the algo is performing task
+    bool sorted;
+
+    int delay;                  // delay in microseconds
+
+    // Setters
+    void setdelay(int);         // Set delay for algorithm loops
     void setalg(int);           // Set Algorithm to use
 
+    // Getters
+    std::string getalg();       // Get currently selected alg name
 };
 
 #endif // ALGORITHM_H
