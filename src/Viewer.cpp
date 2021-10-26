f741c35cc8a7e50aa5fd45957396281bb30ec65d
Bijan Regmi <vizandabijan@gmail.com>
2021-10-26 23:48:42 +0545

    lots of code refactoring

--- src/Viewer.cpp
+++ src/Viewer.cpp
@@ -8,20 +8,29 @@ Viewer::Viewer(int w, int h, Blocks* b){
     height = h;
     blk = b;
 
+    // Create and set textures
     texture.create(width, height);
     this->setTexture(texture.getTexture());
 }
 
 // Renderer
 void Viewer::render(){
-    float r_dx = width/blk->getSize();
+    float r_dx = width/blk->amount;                     // Width of the rectangular block
+    int r_head = blk->r_head;                          // Reader head position
+    int c_head = blk->c_head;                          // Checker head position
 
     texture.clear();
-    for (int i=0; i<blk->getSize(); i++){
+    for (int i=0; i<blk->amount; i++){
         float h = blk->items[i];                        //Using items instead of blk[] so that counters aren't updated
 
         sf::RectangleShape rect(sf::Vector2f(r_dx, h));
         rect.setPosition(i*r_dx, height-h);
+
+        // Highlight the currently reading block
+        if (c_head >= i && c_head > 0)
+            rect.setFillColor(sf::Color::Green);
+        else if (i == r_head)
+            rect.setFillColor(sf::Color::Red);
 
         texture.draw(rect);
     }
--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,92 @@
 #include "Viewer.h"
 #include "blocks.h"
 #include "algorithm.h"
+#include "statusbar.h"
 
 int main(){
-    sf::RenderWindow win(sf::VideoMode(300,300), "SORT");
-    win.setFramerateLimit(60);
+    int WIDTH = 1000;
+    int HEIGHT = 600;
+    int FPS = 60;
+    int DATA_SIZE = 100;
+    int BAR_H = 100;
+
+    sf::RenderWindow win(sf::VideoMode(WIDTH, HEIGHT), "SORT");
+    win.setFramerateLimit(FPS);
     sf::Event ev;
 
-    Blocks data(20,300);
-    Viewer vie(300, 300, &data);
+    Blocks data(DATA_SIZE, HEIGHT-BAR_H);
+    Viewer vie(WIDTH, HEIGHT-BAR_H, &data);
     Algorithms alg(&data);
+    Bar bar(WIDTH, BAR_H, &data, &alg);
+    bar.setPosition(0, HEIGHT-BAR_H);
 
     while(win.isOpen()){
         while(win.pollEvent(ev)){
-            if(ev.type == sf::Event::Closed || (ev.type == sf::Event::KeyPressed && ev.key.code == sf::Keyboard::Escape)){
-                alg.stop();
-                win.close();
-            }
-
+            // Keybindings
             if (ev.type == sf::Event::KeyPressed){
                     switch(ev.key.code){
+                    case sf::Keyboard::Q:
+                    case sf::Keyboard::Escape:
+                        alg.stop();
+                        win.close();
+                        break;
+
                     case sf::Keyboard::Space:
                         alg.start();
                         break;
-                    case sf::Keyboard::S:
+                    case sf::Keyboard::C:
                         alg.setalg(0);
                         break;
-                    case sf::Keyboard::B:
+                    case sf::Keyboard::S:
                         alg.setalg(1);
                         break;
-                    case sf::Keyboard::M:
+
+                    case sf::Keyboard::B:
                         alg.setalg(2);
                         break;
+
+                    case sf::Keyboard::M:
+                        alg.setalg(3);
+                        break;
+
+                    case sf::Keyboard::Add:
+                        if (ev.key.shift && ev.key.control)
+                            alg.setdelay(-5);
+                        else if (ev.key.shift)
+                            alg.setdelay(-3);
+                        else
+                            alg.setdelay(-1);
+                        break;
+
+                    case sf::Keyboard::Subtract:
+                        if (ev.key.shift && ev.key.control)
+                            alg.setdelay(-6);
+                        else if (ev.key.shift)
+                            alg.setdelay(-4);
+                        else
+                            alg.setdelay(-2);
+                        break;
+
+                    case sf::Keyboard::Up:
+                        alg.setalg(-1);
+                        break;
+
+                    case sf::Keyboard::Down:
+                        alg.setalg(-2);
+                        break;
+
                     default:
-                        std::cout << "No keybind set for this key!\n";
+                        std::cout << "No keybind set for this(" << ev.key.code << ") key!\n";
                     }
             }
         }
 
         vie.render();
+        bar.render();
 
         win.clear();
         win.draw(vie);
+        win.draw(bar);
         win.display();
 
     }
