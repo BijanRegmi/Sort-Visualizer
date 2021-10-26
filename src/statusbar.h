8f77808073dae44854eff5547e00910fd6b65316
Bijan Regmi <55084653+BijanRegmi@users.noreply.github.com>
2021-10-26 23:49:06 +0545

    added status bar

---
+++ src/statusbar.h
@@ -0,0 +1,34 @@
+#ifndef STATUSBAR_H
+#define STATUSBAR_H
+
+#include <SFML/Graphics.hpp>
+#include "blocks.h"
+#include "algorithm.h"
+
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+
+class Bar: public sf::Sprite{
+private:
+    sf::RenderTexture texture;  // Texture for sprite
+
+    int width, height;          // Width and height of the sprite
+
+    Blocks* data;               // Pointer to the blocks
+
+    Algorithms* alg;            // Pointer to the algorithm object
+
+    sf::Font font;
+    sf::Text text;
+
+public:
+    // Constructors
+    Bar();
+    Bar(int, int, Blocks*, Algorithms*);
+
+    // Renderer
+    void render();
+};
+
+#endif // STATUSBAR_H
