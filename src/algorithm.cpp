cc4511d3b28f99ff73a6c8daf35297ff4f7dec8d
Bijan Regmi <vizandabijan@gmail.com>
2021-10-30 15:21:24 +0545

    everything working out yahoooooooo

--- src/algorithm.cpp
+++ src/algorithm.cpp
@@ -29,6 +29,7 @@ void Algorithms::setalg(int s){
     else
         selectedAlg = s;
     data.reset_counters();
+    view.unmark_all();
 }
 
 // Data Extractors
@@ -45,9 +46,10 @@ void Algorithms::algo(){
         }
 
         data.reset_counters();
+        view.unmark_all();
         switch(selectedAlg){
         case 0:
-            //Algorithms::check();
+            Algorithms::check();
             break;
         case 1:
             Algorithms::shuffle();
@@ -84,22 +86,23 @@ void Algorithms::algo(){
 // Sorting Algorithm Implementations
 void Algorithms::check(){
     selectedAlg = 0;
+    int n = data.amount-1, c = -1;
     
-    int n = data.amount;
-
-    int c = -1;
-
-    for (c = 0; c < n-1; c++){
+    view.mark(0, 0x00ff00ff);                               // First element is obviously in correct order
+    
+    for (c = 0; c < n; ++c){
         float val = data.items[c];
         if (val > data.items[c+1]){
-            std::cout << "ERROR at " << c << std::endl;
+            view.mark(c+1, 0xff0000ff);                     // Mark red for unsorted elem
             break;
         }
         data.sound.play(2, 0.5+(0.5*val)/data.max_val);
+        view.mark(c, 0x00ff00ff);                           // Mark green for sorted element
         std::this_thread::sleep_for(std::chrono::milliseconds(20));
     }
-    sorted = (n-1 == c) ? true : false;
+    sorted = (n == c) ? true : false;
     if (sorted){
+        view.mark(n, 0x00ff00ff);                           // If sorted mark last element as green
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
         data.sound.play(3, 1);
     } else 
@@ -114,8 +117,10 @@ void Algorithms::shuffle(){
 void Algorithms::bubblesort(){
     for (int i = 0, n = data.amount; i != n-1; ++i){
         for (int j = 0; j < n-i-1; ++j){
+            view.mark(j, 0xff0000ff);               // Mark reading index as red
             if (data.cmp(j, j+1) == 1)
                 data.b_swap(j, j+1);
+            view.unmark(j);                         // Clear the marking
         }
     }
 }
@@ -133,6 +138,8 @@ void Algorithms::m_ms(int left, int right){
     m_merge(left, mid, right);
 }
 void Algorithms::m_merge(int left, int mid, int right){
+    view.mark(left, 0x29f22cff);    // Mark left boundary
+    view.mark(right, 0x08587aff);   // Mark right boundary
     int left_n = mid - left + 1;
     int right_n = right - mid;      // right - (mid + 1) + 1
     
@@ -141,8 +148,16 @@ void Algorithms::m_merge(int left, int mid, int right){
     int right_arr[right_n];
 
     // Copying arrays
-    for (int i=0; i<left_n; ++i) left_arr[i] = data[left+i];
-    for (int i=0; i<right_n; ++i) right_arr[i] = data[mid+1+i];
+    for (int i=0; i<left_n; ++i){
+        view.mark(left+i, 0xff0000ff);               // Mark reading index as red
+        left_arr[i] = data[left+i];
+        view.unmark(left+i);                         // Unmark reading index
+    }
+    for (int i=0; i<right_n; ++i){
+        view.mark(mid+1+i, 0xff0000ff);              // Mark reading index as red
+        right_arr[i] = data[mid+1+i];
+        view.unmark(mid+1+i);                        // Unmark reading index
+    }
 
     // Pointing ints
     int i = 0;
@@ -152,6 +167,7 @@ void Algorithms::m_merge(int left, int mid, int right){
     // Writing to data
     while (i<left_n && j<right_n)
     {
+        view.mark(k, 0xff0000ff);           // Mark writing index as red
         if (left_arr[i] <= right_arr[j]){
             data(k, left_arr[i]);
             i++;
@@ -159,36 +175,50 @@ void Algorithms::m_merge(int left, int mid, int right){
             data(k, right_arr[j]);
             j++; 
         }
+        view.unmark(k);                     // Unmark writing index
         k++;
     }
     while (i<left_n){
+        view.mark(k, 0xff0000ff);           // Mark writing index as red
         data(k, left_arr[i]);
-        i++; k++;
+        i++;
+        view.unmark(k);                     // Unmark writing index
+        k++;
     }
     while (j<right_n){
+        view.mark(k, 0xff0000ff);           // Mark writing index as red
         data(k, right_arr[j]);
-        j++; k++;
+        j++;
+        view.unmark(k);                     // Unmark writing index
+        k++;
     }
+    view.unmark(left);                      // Unmark left boundary
+    view.unmark(right);                     // Unmark right boundary
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void Algorithms::quicksort(){
     q_qs(0, data.amount-1);
 }
 void Algorithms::q_qs(int low, int high){
+    int _q_pivot_index;
     if (low >= high) return;
     _q_pivot_index = q_fix(low, high);
+    view.mark(_q_pivot_index, 0x00ff00ff);              // Mark pivot index as green
 
     q_qs(low, _q_pivot_index-1);
     q_qs(_q_pivot_index+1, high);
+    view.unmark(_q_pivot_index);                        // Unmark pivot index
 }
 int Algorithms::q_fix(int low, int high){
     int i = low - 1;
 
     for (int j = low; j<high; ++j){
+        view.mark(j, 0xff0000ff);               // Mark reading index as red
         if (data.cmp(j, high) == -1){
             i++;
             if (i != j) data.b_swap(i, j);
         }
+        view.unmark(j);                         // Unmark reading index
     }
 
     data.b_swap(i+1, high);
@@ -206,8 +236,11 @@ void Algorithms::r_cs(int place){
     int count[10] = {0};
 
     // Storing how many times a digit appeared at the $(place)th position
-    for (int i=0; i<s; ++i)
+    for (int i=0; i<s; ++i){
+        view.mark(i, 0xff0000ff);                           // Mark reading index as red
         count[ (static_cast<int>(data[i])/place)%10 ]++;
+        view.unmark(i);                                     // Unmark reading index
+    }
 
     // Cummulative adding
     for (int i=1; i<10; ++i)
@@ -220,31 +253,41 @@ void Algorithms::r_cs(int place){
 
     // Filling output array
     for (int i=0; i<s; ++i){
+        view.mark(i, 0xff0000ff);                       // Mark reading index as red
         float val = data[i];
         int digit = (static_cast<int>(val)/place)%10;
         output[ count[digit] ] = val;
         count[digit]++;
+        view.unmark(i);                                 // Unmark reading index
     }
 
     // Filling original array
     for (int i=0; i<s; ++i){
+        view.mark(i, 0x0000ffff);                       // Mark writing index as blue
         data(i, output[i]);
+        view.unmark(i);                                 // Unmark writing index
     }
 }
 int Algorithms::r_max(){
     int max_i = 0;
-    for (int i=0, j=data.amount; i<j; ++i)
+    for (int i=0, j=data.amount; i<j; ++i){
+        view.mark(i, 0xff0000ff);                       // Mark reading index as red
         if (data.cmp(i, max_i) == 1) max_i = i;
+        view.unmark(i);                                 // Unmark reading index
+    }
     return data[max_i];
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void Algorithms::insertionsort(){
     for (int i = 1, k=data.amount; i<k; ++i){
+        view.mark(i, 0xff0000ff);
         int curr = data[i];
         int j = i - 1;
 
         while (j>=0 && data[j] > curr){
+            view.mark(j, 0x0000ffff);
             data(j+1, data[j]);
+            view.unmark(j);
             j--;
         }
         data(j+1, curr);
@@ -257,11 +300,18 @@ void Algorithms::selectionsort(){
         min_val_index = i;
 
         // Finding the minimum value index
-        for (int j=i+1; j<n-1; ++j)
+        for (int j=i+1; j<n-1; ++j){
+            view.mark(j, 0xff0000ff);
             if (data.cmp(j, min_val_index) == -1)
                 min_val_index = j;
+            view.unmark(j);
+        }
         
-        if (min_val_index != i) data.b_swap(i, min_val_index);
+        if (min_val_index != i){
+            view.mark(i, 0x0000ffff);
+            data.b_swap(i, min_val_index);
+            view.unmark(i);
+        }
     }
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
